192cab53083866e73d909347e4ba5d59fb3947ce
ABHISHEK KUMAR <128277852+k-abhi1@users.noreply.github.com>
2025-04-02 22:35:32 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,12 @@
+let a=prompt("enter the first number");
+console.log(a);
+
+
+// type of variable
+// let b=prompt("enter the second number");     under function scope
+// var c=prompt("enter the third number");      global variable
+// const d=prompt("enter the fourth number");    const variable not change in value
+
+// console.log(b);
+// console.log(c);
+// console.log(d);
